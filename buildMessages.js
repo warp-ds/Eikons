c572613a9915cccab9a8dcd7270121f0ecc4fd96
zaramelyan <zarameljan@gmail.com>
2025-08-28 14:18:15 +0200

    feat: add .yml files for crowdin

--- buildMessages.js
+++ buildMessages.js
@@ -5,12 +5,11 @@ import { messages as da } from "./locales/da/messages.mjs";
 import { messages as sv } from "./locales/sv/messages.mjs";
 
 export const buildMessages = async (iconId) => {
-
-return {
-  nb: nb[iconId],
-  en: en[iconId],
-  fi: fi[iconId],
-  da: da[iconId],
-  sv: sv[iconId],
+	return {
+		nb: nb[iconId],
+		en: en[iconId],
+		fi: fi[iconId],
+		da: da[iconId],
+		sv: sv[iconId],
+	};
 };
-}
